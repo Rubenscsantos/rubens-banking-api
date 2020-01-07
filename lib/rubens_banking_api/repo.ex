c30d5691f457afd64c233a32be8ea3fe7e22b872
Rubens <contato.rubenscsantos@gmail.com>
2020-01-06 21:09:16 -0300

    WIP

--- lib/rubens_banking_api/repo.ex
+++ lib/rubens_banking_api/repo.ex
@@ -6,6 +6,14 @@ defmodule RubensBankingApi.Repo do
   DATABASE_URL environment variable.
   """
   def init(_, opts) do
-    {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
+    config =
+      opts
+      |> Keyword.put(:username, System.get_env("PGUSER"))
+      |> Keyword.put(:password, System.get_env("PGPASSWORD"))
+      |> Keyword.put(:database, System.get_env("PGDATABASE"))
+      |> Keyword.put(:hostname, System.get_env("PGHOST"))
+      |> Keyword.put(:port, System.get_env("PGPORT") |> String.to_integer())
+
+    {:ok, config}
   end
 end
