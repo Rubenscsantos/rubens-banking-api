3514c18b5f9d87b660f54fce94dd2b314ee071f2
Rubens <contato.rubenscsantos@gmail.com>
2020-01-07 01:51:40 -0300

    feat: Add endpoints

--- lib/rubens_banking_api_web/router.ex
+++ lib/rubens_banking_api_web/router.ex
@@ -1,22 +1,35 @@
 defmodule RubensBankingApiWeb.Router do
   use RubensBankingApiWeb, :router
 
+  alias RubensBankingApiWeb.AccountsController
+
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", RubensBankingApiWeb do
-    pipe_through :browser # Use the default browser stack
+    # Use the default browser stack
+    pipe_through(:browser)
 
-    get "/", PageController, :index
+    get("/", PageController, :index)
+  end
+
+  scope "/api/v1" do
+    pipe_through(:api)
+
+    scope "/accounts", RubensBankingApiWeb do
+      post("/", AccountsController, :create)
+      get("/", AccountsController, :show_all)
+      get("/:account_id", AccountsController, :show)
+    end
   end
 
   # Other scopes may use custom stacks.
